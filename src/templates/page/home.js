b256ea86f611581e0f745f8e2cf97cbd0f90ade4
Jay <thinnakrit.idesign@gmail.com>
2021-06-07 15:05:33 +0700

    Add example template

---
+++ src/templates/page/home.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+//
+import { colors } from 'templates'
+//
+const PageHomeTemplate = {
+  PageWrapper: styled.div`
+    padding-top: 80px;
+    b {
+      ${colors.primary}
+    }
+
+    .example-class {
+      .sub-class {
+        img {
+          /*  */
+        }
+      }
+    }
+  `,
+}
+
+export default PageHomeTemplate
