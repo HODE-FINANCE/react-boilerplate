cb8d39232ae4db6286720df45e2613077f20accb
Jay <thinnakrit.idesign@gmail.com>
2021-06-10 10:24:27 +0700

    Add example call contract

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import ethers from 'ethers'
 import { useIntl } from 'react-intl'
+//
+import useEther from 'hooks/useEther'
 //
 function HomePage() {
   const intl = useIntl()
+  const { ether } = useEther()
+  const handleCallContract = async () => {
+    const contract = new ethers.Contract('CONTRACT_ADDR', 'ABI', ether.provider)
+  }
+
   return <div>{intl.formatMessage({ id: 'home.welcome' })}</div>
 }
 
